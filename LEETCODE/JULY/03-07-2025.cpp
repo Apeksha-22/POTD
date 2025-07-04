79ffcc17193afc7720fa6b67a100f30ebf73b553
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-07-04 12:36:44 +0530

    Update 03-07-2025.cpp

--- LEETCODE/JULY/03-07-2025.cpp
+++ LEETCODE/JULY/03-07-2025.cpp
@@ -4,26 +4,6 @@
 class Solution {
 public:
     char kthCharacter(int k) {
-        int ans = 0;
-        int t;
-        while (k != 1) {
-            t = __lg(k);
-            if ((1 << t) == k) {
-                t--;
-            }
-            k = k - (1 << t);
-            ans++;
-        }
-        return 'a' + ans;
-    }
-};
-
-
-// one more solution
-
-class Solution {
-public:
-    char kthCharacter(int k) {
         string s = "a";
         int i=0;
         while(s.size() < k){
@@ -37,5 +17,25 @@ public:
             s += x;
         }
         return s[k-1];
+    }
+};
+
+
+
+// one more solution
+class Solution {
+public:
+    char kthCharacter(int k) {
+        int ans = 0;
+        int t;
+        while (k != 1) {
+            t = __lg(k);
+            if ((1 << t) == k) {
+                t--;
+            }
+            k = k - (1 << t);
+            ans++;
+        }
+        return 'a' + ans;
     }
 };
