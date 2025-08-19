50e8e0b581fbf4665a317078940c3c6319af6b18
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-08-20 00:41:26 +0530

    Update 19-08-2025.cpp

--- GFG/AUGUST/19-08-2025.cpp
+++ GFG/AUGUST/19-08-2025.cpp
@@ -2,6 +2,39 @@
 
 
 
+class Solution {
+  public:
+    vector<int> farMin(vector<int>& arr) {
+        // code here
+        int n=arr.size();
+        vector<int>suffmin(n), ans(n,-1);
+        
+        suffmin[n-1] = arr[n-1];
+        for(int i=n-2; i>=0; i--){
+            suffmin[i] = min(arr[i], suffmin[i+1]);
+        }
+        
+        for(int i=0; i<n; i++){
+            
+            int s = i+1, e=n-1;
+            
+            while(s<=e){
+                int mid = s+(e-s)/2;
+                
+                if(suffmin[mid] < arr[i]){
+                    ans[i] = mid;
+                    s = mid + 1;
+                }else{
+                    e = mid - 1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+
+//2nd Method
 
 class Solution {
   public:
