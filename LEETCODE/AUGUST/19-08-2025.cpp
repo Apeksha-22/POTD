d87cb6540f5160c918fc8880d76811201b6ded93
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-08-19 23:27:26 +0530

    Create 19-08-2025.cpp

---
+++ LEETCODE/AUGUST/19-08-2025.cpp
@@ -0,0 +1,20 @@
+// 2348. Number of Zero-Filled Subarrays
+
+
+
+
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long cnt = 0, streak = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                streak++;
+                cnt += streak;
+            } else {
+                streak = 0;
+            }
+        }
+        return cnt;
+    }
+};
