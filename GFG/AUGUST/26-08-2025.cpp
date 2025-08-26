eb95c645b596a46a235d87a177b19f4a8a52f8e2
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-08-26 19:21:58 +0530

    Create 26-08-2025.cpp

---
+++ GFG/AUGUST/26-08-2025.cpp
@@ -0,0 +1,23 @@
+//Check if a String is Subsequence of Other
+
+
+
+class Solution {
+  public:
+    bool isSubSeq(string& s1, string& s2) {
+        // code here
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < s1.length() && j < s2.length()) {
+            
+            if(s1[i] == s2[j]) {
+                i++;
+            }
+            j++;
+        }
+        
+        return i == s1.length();
+    }
+};
