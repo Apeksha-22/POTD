eb0465cf708a5663dfc3d3a606531b6c0a40447e
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-08-31 21:42:08 +0530

    Create 31-08-2025.cpp

---
+++ GFG/AUGUST/31-08-2025.cpp
@@ -0,0 +1,26 @@
+//Container With Most Water
+
+
+
+
+int maxWater(vector<int> &arr) {
+        // code here
+        int left = 0;
+        int right = arr.size() - 1;
+        int area = 0;
+        
+        while(left<=right){
+            area = max(area, (right - left)*min(arr[right], arr[left]));
+            
+            if(arr[left]<arr[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+            
+        }
+        
+        return area;
+        
+    }
