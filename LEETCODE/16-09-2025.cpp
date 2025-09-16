bc3c52d7d1a5c56bf7e371fa8ebd5b51f3535f77
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-09-16 22:07:14 +0530

    Create 16-09-2025.cpp

---
+++ LEETCODE/16-09-2025.cpp
@@ -0,0 +1,30 @@
+// 2197. Replace Non-Coprime Numbers in Array
+
+
+
+
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> st;
+        for (int num : nums) {
+            while (!st.empty()) {
+                int top = st.back();
+                int g = gcd(top, num);
+                if (g == 1) {
+                    break;
+                }
+                st.pop_back();
+                long long merged = (long long)top / g * num;
+                num = (int)merged;
+            }
+            st.push_back(num);
+        }
+        return st;
+    }
+    
+private:
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+};
