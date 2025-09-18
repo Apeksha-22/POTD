647dbed35e9a481f61e2776520e94c866f1812f6
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-09-18 21:37:45 +0530

    Create 18-09-2025.cpp

---
+++ GFG/SEPTEMBER/18-09-2025.cpp
@@ -0,0 +1,25 @@
+// Next Greater Element in Circular Array
+
+
+
+
+class Solution {
+  public:
+    vector<int> nextGreater(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        vector<int>res(n,-1);
+        stack<int>st;
+        
+        int i=0;
+        while(i < 2*n){
+            while(!st.empty() && arr[st.top()] < arr[i%n]){
+                res[st.top()] = arr[i%n];
+                st.pop();
+            }
+            st.push(i%n) , i++;
+        }
+        
+        return res;
+    }
+};
