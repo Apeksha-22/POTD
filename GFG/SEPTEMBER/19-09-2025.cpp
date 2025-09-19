57ae6530a295360fac46652de7ee6dd2778d201d
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-09-19 22:47:44 +0530

    Create 19-09-2025.cpp

---
+++ GFG/SEPTEMBER/19-09-2025.cpp
@@ -0,0 +1,21 @@
+// Min Add to Make Parentheses Valid
+
+
+
+
+class Solution {
+  public:
+    int minParentheses(string& s) {
+        stack<int> o,c;
+        
+        for(auto it:s){
+            if(it=='(') o.push(it);
+            else{
+                if(o.empty()) c.push(it);
+                else o.pop();
+            }
+        }
+        
+        return c.size()+o.size();
+    }
+};
