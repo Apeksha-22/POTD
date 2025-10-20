910559835ccb3d1577316d547e0f2f57d4825d42
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-10-20 18:30:00 +0530

    Add solution for final value of variable problem

---
+++ LEETCODE/OCTOBER/20-10-2025.cpp
@@ -0,0 +1,19 @@
+// 2011. Final Value of Variable After Performing Operations
+
+
+
+
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (auto& op : operations) {
+            if (op == "X++" || op == "++X") {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        return x;
+    }
+};
