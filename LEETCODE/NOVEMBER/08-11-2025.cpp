5e2bb5367992d97ae1cb8613c7efffa7aaec0e87
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-11-08 21:03:20 +0530

    Create 08-11-2025.cpp

---
+++ LEETCODE/NOVEMBER/08-11-2025.cpp
@@ -0,0 +1,15 @@
+// 1611. Minimum One Bit Operations to Make Integers Zero
+
+
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int res = 0;
+        while (n) {
+            res ^= n;
+            n >>= 1;
+        }
+        return res;
+    }
+};
