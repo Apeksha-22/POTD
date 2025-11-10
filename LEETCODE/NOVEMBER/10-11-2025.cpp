e5e64676978dfa50a3a1e95a2f94928e5939d2e0
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-11-10 22:07:01 +0530

    Create 10-11-2025.cpp

---
+++ LEETCODE/NOVEMBER/10-11-2025.cpp
@@ -0,0 +1,20 @@
+// 3542. Minimum Operations to Convert All Elements to Zero
+
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        vector<int> stack(nums.size() + 1, 0);
+        int top = 0, ans = 0;
+        for (int num : nums) {
+            while (stack[top] > num) {
+                top--;
+                ans++;
+            }
+            if (stack[top] != num)
+                stack[++top] = num;
+        }
+        return ans + top;
+    }
+};
