54f9286dbe38b37ccd52d46b014b8903122ba581
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-11-22 21:47:26 +0530

    Create 22-11-2025.cpp

---
+++ LEETCODE/NOVEMBER/22-11-2025.cpp
@@ -0,0 +1,12 @@
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three
+
+
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        return std::accumulate(nums.begin(), nums.end(), 0, [](int acc, int v) {
+            return acc + std::min(v % 3, 3 - v % 3);
+        });
+    }
+};
