974ddd2dbf766bb4394ad4ff28100933871d8b19
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-11-27 23:50:20 +0530

    Create 27-11-2025.cpp

---
+++ GFG/NOVEMBER/27-11-2025.cpp
@@ -0,0 +1,15 @@
+// All Subsets Xor Sum
+
+
+
+
+class Solution {
+  public:
+  
+    int subsetXORSum(vector<int>& arr) {
+        // code here
+         int OR =0 , n = arr.size();
+         for(int x : arr) OR |= x;
+         return OR * (pow(2,n-1)); 
+    }
+};
