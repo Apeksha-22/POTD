9a3c3134e1dad83edcbd481be04eb559ff629636
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-12-05 19:45:04 +0530

    Create 05-12-2025.cpp

---
+++ LEETCODE/DECEMBER/05-12-2025.cpp
@@ -0,0 +1,14 @@
+// 3432. Count Partitions with Even Sum Difference
+
+
+
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int totalSum = 0;
+        for (int x : nums) {
+            totalSum += x;
+        }
+        return totalSum % 2 == 0 ? nums.size() - 1 : 0;
+    }
+};
