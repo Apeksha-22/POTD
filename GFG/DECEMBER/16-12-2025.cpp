f106fea456902fec06c8948f8df16e857215ce8f
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-12-16 07:31:18 +0530

    Add function to check string rotations
    Implement function to check if two strings are rotations of each other.

---
+++ GFG/DECEMBER/16-12-2025.cpp
@@ -0,0 +1,15 @@
+//Strings Rotations of Each Other
+
+
+
+class Solution {
+  public:
+    bool areRotations(string &s1, string &s2) {
+        // code here
+        if (s1.length() != s2.length())
+        return false;
+
+    string t = s1 + s1;
+    return t.find(s2) < t.length();
+    }
+};   
