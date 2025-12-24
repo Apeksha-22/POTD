6460ff1ef28f979f50909279bba792f4f366eee1
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-12-24 13:19:30 +0530

    Create 24-12-2025.cpp

---
+++ GFG/DECEMBER/24-12-2025.cpp
@@ -0,0 +1,19 @@
+// Count elements less than or equal to k in a sorted rotated array
+
+
+
+class Solution {
+    public:
+    int countLessEqual(vector<int>& arr, int x) {
+        // code here
+        int cnt=0;
+        for(int num:arr)
+        {
+            if(num<=x)
+            {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+};
