15cedf784137a607ef1bba6b678cc472b05497fd
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-12-24 13:21:39 +0530

    Create 24-12-2025.cpp

---
+++ LEETCODE/DECEMBER/24-12-2025.cpp
@@ -0,0 +1,17 @@
+//3074. Apple Redistribution into Boxes
+
+
+
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& cap) {
+        int sum = accumulate(apple.begin(), apple.end(), 0);
+        sort(cap.begin(), cap.end(), greater<int>());
+
+        int res = 0;
+        while (sum > 0)
+            sum -= cap[res++];
+
+        return res;
+    }
+};
