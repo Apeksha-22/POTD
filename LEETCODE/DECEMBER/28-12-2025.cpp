aa77eb492e66aed38c1f2f3e58db30c0b8750216
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2025-12-28 12:54:43 +0530

    Create 28-12-2025.cpp

---
+++ LEETCODE/DECEMBER/28-12-2025.cpp
@@ -0,0 +1,23 @@
+// 1351. Count Negative Numbers in a Sorted Matrix
+
+
+
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int i = m - 1, j = 0;
+
+        int res = 0;
+
+        while (i >= 0 && j < n) {
+            if (grid[i][j] < 0) {
+                res += n - j;
+                i--;
+            } else
+                j++;
+        }
+
+        return res;
+    }
+};
