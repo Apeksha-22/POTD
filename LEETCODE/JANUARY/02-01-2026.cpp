4edcbc66eb0a549fd214ac084c0bd76bf9c685a3
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-01-02 18:56:44 +0530

    Create 02-01-2026.cpp

---
+++ LEETCODE/JANUARY/02-01-2026.cpp
@@ -0,0 +1,13 @@
+//961. N-Repeated Element in Size 2N Array
+
+
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& A) {
+        for (int i = 0; i < A.size() - 2; ++i)
+            if (A[i] == A[i + 1] || A[i] == A[i + 2])
+                return A[i];
+        return A.back();
+    }
+};
