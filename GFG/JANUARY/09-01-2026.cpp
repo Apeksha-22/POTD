02ae1c713592e72671746d4fb9d6b0522e47a782
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-01-09 22:11:27 +0530

    Create 09-01-2026.cpp

---
+++ GFG/JANUARY/09-01-2026.cpp
@@ -0,0 +1,27 @@
+// Subarrays With At Most K Distinct Integers
+
+
+
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        // code here
+        unordered_map<int, int> mp;
+        
+        int l=0, ans=0;
+        
+        for (int r=0; r<arr.size(); r++) {
+            mp[arr[r]]++;
+            
+            while (mp.size()>k) {
+                mp[arr[l]]--;
+                if (mp[arr[l]] == 0) mp.erase(arr[l]);
+                l++;
+            }
+            
+            ans += r-l+1;
+        }
+        
+        return ans;
+    }
+};
