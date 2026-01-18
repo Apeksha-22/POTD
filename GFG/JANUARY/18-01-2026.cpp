46ea1607a7c01ba736b0085bfeb2eb14be1653b6
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-01-18 21:47:52 +0530

    Add solution for next element with greater frequency

---
+++ GFG/JANUARY/18-01-2026.cpp
@@ -0,0 +1,26 @@
+// Next element with greater frequency
+
+
+
+class Solution {
+  public:
+    vector<int> nextFreqGreater(vector<int>& arr) {
+        int n = arr.size();
+        map<int, int> freq;
+
+        for (auto it : arr) {
+            freq[it]++;
+        }
+        vector<int> res(n, -1);
+        stack<int> s;
+
+        for (int i = 0; i < n; i++) {
+            while (!s.empty() && freq[arr[i]] > freq[arr[s.top()]]) {
+                res[s.top()] = arr[i];
+                s.pop();
+            }
+            s.push(i);
+        }
+        return res;
+    }
+};
