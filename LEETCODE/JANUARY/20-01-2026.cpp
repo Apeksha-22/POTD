0d20d96ceb6deefa04406eeecce8dec2d3d76c07
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-01-20 11:44:05 +0530

    Create 20-01-2026.cpp

---
+++ LEETCODE/JANUARY/20-01-2026.cpp
@@ -0,0 +1,18 @@
+// 3314. Construct the Minimum Bitwise Array I
+
+
+
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> res;
+        for (int n : nums) {
+            if (n & 1) {
+                int z = ((n + 1) & ~n) >> 1;
+                res.push_back(n & ~z);
+            } else
+                res.push_back(-1);
+        }
+        return res;
+    }
+};
