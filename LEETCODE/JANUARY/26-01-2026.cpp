8ce10dadb6445f3a03b34043e5efbeac6c6d8c89
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-01-26 21:10:18 +0530

    Update 26-01-2026.cpp

--- LEETCODE/JANUARY/26-01-2026.cpp
+++ LEETCODE/JANUARY/26-01-2026.cpp
@@ -4,18 +4,18 @@
 
 class Solution {
 public:
-    static vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        const int n=arr.size();
-        int minD=1e9+7;
-        for(int i=1; i<n; i++){
-            minD=min(minD, arr[i]-arr[i-1]);
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        int diff=INT_MAX;
+        vector<vector<int>>res;
+        for(int i=0;i<n-1;i++){
+            if(arr[i+1] - arr[i] < diff){
+                res.clear();
+                diff=arr[i+1]-arr[i];
+            }else if(arr[i+1]-arr[i]>diff) continue;
+            res.push_back({arr[i],arr[i+1]});
         }
-        vector<vector<int>> ans;
-        for(int i=1; i<n; i++){
-            if (arr[i]-arr[i-1]==minD)
-                ans.push_back({arr[i-1], arr[i]});
-        }
-        return ans;
+        return res;
     }
 };
