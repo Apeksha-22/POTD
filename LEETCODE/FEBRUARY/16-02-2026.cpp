74fe50860567414c5ddd10c3ba10f6b59d61a125
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-02-16 06:44:46 +0530

    Create 16-02-2026.cpp

---
+++ LEETCODE/FEBRUARY/16-02-2026.cpp
@@ -0,0 +1,16 @@
+// 190. Reverse Bits
+
+
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        bitset<32> B(n);
+        for(int i=0; i<16; i++){
+            B[i]=B[i]^B[31-i];
+            B[31-i]=B[i]^B[31-i];
+            B[i]=B[i]^B[31-i];
+        }     
+        return B.to_ulong();
+    }
+};
