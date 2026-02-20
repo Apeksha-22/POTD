429950d3f3451e1e19ea46335dee7682b825ac86
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-02-20 22:23:12 +0530

    Create 20-02-2026.cpp

---
+++ GFG/FEBRUARY/20-02-2026.cpp
@@ -0,0 +1,24 @@
+// Form the Largest Number
+
+
+class Solution {
+  public:
+    static bool func(string &a, string &b){
+        return a+b > b+a;
+    }
+    string findLargest(vector<int> &nums) {
+        // code here
+        vector<string>arr;
+        string ans = "";
+        for(int x : nums)
+        arr.push_back(to_string(x));
+        
+        sort(arr.begin(),arr.end(),func);
+        
+        for(auto & c : arr)
+        ans += c;
+        
+        if(ans[0] == '0') return "0";
+        return ans;
+    }
+};
