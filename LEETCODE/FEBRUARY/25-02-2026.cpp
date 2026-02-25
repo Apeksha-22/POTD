ac02376e355a527175fa50762eb7a072406437d2
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-02-25 20:22:37 +0530

    Create 25-02-2026.cpp

---
+++ LEETCODE/FEBRUARY/25-02-2026.cpp
@@ -0,0 +1,17 @@
+// 1356. Sort Integers by The Number of 1 Bits
+
+
+
+class Solution {
+public:
+    static bool compare(int a, int b) {
+        if (__builtin_popcount(a) == __builtin_popcount(b)) {
+            return a < b;
+        }
+        return __builtin_popcount(a) < __builtin_popcount(b);
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(), arr.end(), compare);
+        return arr;
+    }
+};
