388e7a6e5987cbf02db351d79f0a83f39b01a6e1
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-02-28 20:38:06 +0530

    Create 28-02-2026.cpp

---
+++ LEETCODE/FEBRUARY/28-02-2026.cpp
@@ -0,0 +1,18 @@
+// 1680. Concatenation of Consecutive Binary Numbers
+
+
+
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long res = 0;
+        int bits = 0;
+
+        for(int i=1; i<=n; i++){
+            if((i & (i-1)) == 0) bits++;
+            res = ((res<<bits) | i) % MOD;
+        }
+        return res;
+    }
+};
