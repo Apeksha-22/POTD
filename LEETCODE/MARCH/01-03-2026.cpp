17bfa263f8c2a8f0c352eb8d309ace0aa0f2ff1d
Apeksha Jain <168848064+Apeksha-22@users.noreply.github.com>
2026-03-01 08:20:29 +0530

    Create 01-03-2026.cpp

---
+++ LEETCODE/MARCH/01-03-2026.cpp
@@ -0,0 +1,12 @@
+// 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+
+
+
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ans = 0;
+        for (char& c : n) ans = max(ans, c - '0');
+        return ans;
+    }
+};
