4a4486e15f1263c4ae16ffd94c573b655256d2b9
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-05 21:04:30 +0530

    Create 05-04-2026.cpp

---
+++ LEETCODE/APRIL/05-04-2026.cpp
@@ -0,0 +1,13 @@
+// 657. Robot Return to Origin
+
+
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        if (moves.length() & 1) return false;
+        unordered_map<char, int> f;
+        for (char c : moves) f[c]++;
+        return f['U'] == f['D'] && f['L'] == f['R'];
+    }
+};
