012cb700846d37e18b7f74305fd0ee431de6156f
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-12 19:11:23 +0530

    Create 12-04-2026.cpp

---
+++ GFG/APRIL/12-04-2026.cpp
@@ -0,0 +1,15 @@
+// Toeplitz matrix
+
+
+class Solution {
+  public:
+    bool isToeplitz(vector<vector<int>>& mat) {
+        // code here
+        for(int i=1; i<mat.size(); i++){
+        for(int j=1; j<mat[0].size(); j++){
+            if(mat[i][j] != mat[i-1][j-1]){return 0;}
+          }
+       }
+      return 1;
+    }
+};
