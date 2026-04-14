80e6b49a42c3a6d6282ea9184c478dffc3236a6f
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-14 22:45:00 +0530

    Add function to remove spaces from a string
    Implement a function to remove spaces from a string.

---
+++ GFG/APRIL/14-04-2026.cpp
@@ -0,0 +1,17 @@
+// Remove Spaces
+
+
+
+class Solution {
+  public:
+    string removeSpaces(string& s) {
+        // code here
+        string ans = "";
+        for(int i=0; i<s.length(); i++){
+            if(s[i] != ' '){
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
