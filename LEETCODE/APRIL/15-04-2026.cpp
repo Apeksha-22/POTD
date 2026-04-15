a0897aa0cfe6b6dac2e31fdb2e4c36d8b22d0cea
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-15 22:57:09 +0530

    Create 15-04-2026.cpp

---
+++ LEETCODE/APRIL/15-04-2026.cpp
@@ -0,0 +1,15 @@
+// 2515. Shortest Distance to Target String in a Circular Array
+
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int start) {
+        int n = words.size();
+        for (int i = 0; i <= n >> 1; i++)
+            if (words[(start + i) % n] == target |
+                words[(start - i + n) % n] == target)
+                return i;
+
+        return -1;
+    }
+};
