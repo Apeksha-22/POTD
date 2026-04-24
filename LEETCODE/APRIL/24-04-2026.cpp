cfe3ca56cfcd04209319fcad846e9682dd3c9ce6
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-24 07:54:37 +0530

    Create 24-04-2026.cpp

---
+++ LEETCODE/APRIL/24-04-2026.cpp
@@ -0,0 +1,20 @@
+// 2833. Furthest Point From Origin
+
+
+
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int L = 0, R = 0, B = 0;
+        for (auto c : moves) {
+            if (c == 'L') {
+                L++;
+            } else if (c == 'R') {
+                R++;
+            } else {
+                B++;
+            }
+        }
+        return abs(L - R) + B;
+    }
+};
