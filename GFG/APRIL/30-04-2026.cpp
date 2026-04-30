1736a023024453425b0ef9d7ce6351df7c411729
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-04-30 23:40:07 +0530

    Create 30-04-2026.cpp

---
+++ GFG/APRIL/30-04-2026.cpp
@@ -0,0 +1,28 @@
+// Check if an Array is Max Heap
+
+
+
+class Solution {
+  public:
+    bool isMaxHeap(vector<int> &arr) {
+        // code
+        int n = arr.size();
+        
+        for(int i=0; i<n; i++)
+        {
+            int left_child = i*2 + 1;
+            int right_child = i*2 + 2;
+            
+            if(left_child < n && arr[left_child] > arr[i])
+            {
+                return false;
+            }
+            if(right_child < n && arr[right_child] > arr[i])
+            {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
