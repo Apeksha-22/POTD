0c7f6ca0074cc434cd97820a0d41cac909f038eb
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-05-02 20:52:27 +0530

    Create 02-05-2026.cpp

---
+++ GFG/MAY/02-05-2026.cpp
@@ -0,0 +1,14 @@
+// Position of the Set Bit
+
+
+
+class Solution {
+  public:
+    int findPosition(int n) {
+        // code here
+        if(n &(n-1)){
+            return -1;
+        }
+        return __builtin_popcount(n-1)+1;
+    }
+};
