e1f3650d1b062c19c11fcf0a4b6891a9dbe6bf70
Apeksha Jain <morenaapekshajain2209@gmail.com>
2026-05-16 23:28:37 +0530

    Create 16-05-2026.cpp

---
+++ GFG/MAY/16-05-2026.cpp
@@ -0,0 +1,20 @@
+// Not a subset sum
+
+
+
+class Solution {
+public:
+    int findSmallest(std::vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        
+        long long res = 1;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] > res) {
+                break;
+            }
+            res += arr[i];
+        }
+        return res;
+    }
+};
